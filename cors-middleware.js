66d37b2ce41d0be4834cd3516b56e31992cf380a
Erlimar Silva Campos <erlimar@gmail.com>
2018-08-14 12:29:55 -0300

    Ajusta CORS para ser mais permissivo

--- cors-middleware.js
+++ cors-middleware.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     return (req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "*");
         next();
     }
 }
